fa66839424ff584465ef3b2b30731eba38ae369e
Jasper Kooij <jasperkooij@gmail.com>
2024-12-06 12:00:15 -0600

    Add GA4

---
+++ src/hooks.server.js
@@ -0,0 +1,18 @@
+export async function handle({ event, resolve }) {
+	const response = await resolve(event, {
+		transformPageChunk: ({ html }) =>
+			html.replace(
+				'</head>',
+				`<script async src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"></script>
+         <script>
+           window.dataLayer = window.dataLayer || [];
+           function gtag(){dataLayer.push(arguments);}
+           gtag('js', new Date());
+           gtag('config', 'G-FN7H397ES2');
+         </script>
+        </head>`
+			)
+	});
+
+	return response;
+}
